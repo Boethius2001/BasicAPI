6f2f77f46031dc6a70268f43dec77ae32d8845e6
Boethius2001 <emrecan.senel2001@gmail.com>
2025-02-18 15:45:26 +0300

    test2

--- BasicAPI/Program.cs
+++ BasicAPI/Program.cs
@@ -1,19 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "go /books");
 
 List<Book> books = new()
 {
-    new Book {id = 1, Title="Lord of the rings", Author="Tolkien" }
+    new Book {id = 1, Title="Yüzüklerin Efendisi", Author="J.R.R" },
+    new Book {id = 2, Title="Yüksek satodaki adam", Author="Philip kindred dick" }
 };
 
+// GET request 
+app.MapGet("/books", () => books);
 
 app.Run();
 
 class Book
 {
-    public int id;
-    public string Title;
-    public string Author;
+    public int id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
 }
