b56e510864b068b4bbf15ee9e42d9a247d630391
Boethius2001 <emrecan.senel2001@gmail.com>
2025-02-18 15:53:09 +0300

    get

--- BasicAPI/Program.cs
+++ BasicAPI/Program.cs
@@ -6,11 +6,18 @@ app.MapGet("/", () => "go /books");
 List<Book> books = new()
 {
     new Book {id = 1, Title="Yüzüklerin Efendisi", Author="J.R.R" },
-    new Book {id = 2, Title="Yüksek satodaki adam", Author="Philip kindred dick" }
+    new Book {id = 2, Title="Yüksek satodaki adam", Author="Philip Kindred Dick" }
 };
 
-// GET request 
+// bütün kitap verilerini döndürür
 app.MapGet("/books", () => books);
+
+// belirtilen id ye göre veriyi döndürür
+app.MapGet("/books/{id}", (int id) =>
+{
+    var book = books.FirstOrDefault(book => book.id == id); // eþleþen id ye göre veriyi depola
+    return book;
+});
 
 app.Run();
 
