ad5f66f652c81d411d72ff4ff33ac90bc56e6a27
Boethius2001 <emrecan.senel2001@gmail.com>
2025-02-18 15:37:15 +0300

    test

--- BasicAPI/Program.cs
+++ BasicAPI/Program.cs
@@ -3,4 +3,17 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+List<Book> books = new()
+{
+    new Book {id = 1, Title="Lord of the rings", Author="Tolkien" }
+};
+
+
 app.Run();
+
+class Book
+{
+    public int id;
+    public string Title;
+    public string Author;
+}
