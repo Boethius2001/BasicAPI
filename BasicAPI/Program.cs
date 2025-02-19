1aae76c43c825b4315bc3085be7ebf031258b1cf
Boethius2001 <emrecan.senel2001@gmail.com>
2025-02-19 12:16:17 +0300

    update and delete

--- BasicAPI/Program.cs
+++ BasicAPI/Program.cs
@@ -15,7 +15,7 @@ app.MapGet("/books", () => books);
 // belirtilen id ye göre veriyi döndürür
 app.MapGet("/books/{id}", (int id) =>
 {
-    var book = books.FirstOrDefault(book => book.id == id); // eslesen id ye göre veriyi depola
+    var book = books.FirstOrDefault(b => b.id == id); // eslesen id ye göre veriyi depola
     return book;
 });
 
@@ -24,6 +24,24 @@ app.MapPost("/books", (Book book) =>
 {
     book.id = books.Max(b => b.id) + 1; //sayý olarak en büyük id ye 1 ekler
     books.Add(book);
+    return Results.Redirect("/books");
+});
+
+// kitap verisini güncellemek
+app.MapPut("/books/{id}", (int id, Book UpdatedBook) =>
+{
+    var OldBook = books.FirstOrDefault(b => b.id == id);
+
+    OldBook.Title = UpdatedBook.Title;
+    OldBook.Author = UpdatedBook.Author;
+    return Results.Redirect("/books");
+});
+
+// kitap verisini silmek
+app.MapDelete("/books/{id}", (int id) =>
+{
+    var book = books.FirstOrDefault(b => b.id == id);
+    books.Remove(book);
     return Results.Redirect("/books");
 });
 
