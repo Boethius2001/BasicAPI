e6792dcaa68617f13e35a4585aef6d6e37120c80
Boethius2001 <emrecan.senel2001@gmail.com>
2025-02-23 13:42:37 +0300

    db context

---
+++ BasicAPI/BookContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BasicAPI
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; } // Kitap objelerinin koleksiyonu
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source=Library.db");
+    }
+}
--- BasicAPI/Program.cs
+++ BasicAPI/Program.cs
@@ -48,9 +48,11 @@ app.MapDelete("/books/{id}", (int id) =>
 
 app.Run();
 
+/*
 class Book
 {
     public int id { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
 }
+*/
